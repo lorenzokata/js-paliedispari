3ea1236bb2524e78daf1ad1bb7ab56fc5dfe2b58
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-06-07 16:43:02 +0200

    ordinato codice es pariDispari

--- js/script.js
+++ js/script.js
@@ -1,31 +1,16 @@
 // Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 sceltaUtente = prompt("pari o dispari?");
-console.log(sceltaUtente);
 numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
-console.log(numeroUtente);
 numeroComputer = randomNumber1to5();
-console.log(numeroComputer);
 somma = numeroUtente + numeroComputer;
 risultato = pariDispari(somma);
-console.log(risultato);
 
 if (sceltaUtente == risultato) {
     console.log("Hai vinto!!");
 } else {
     console.log("Hai perso!!");
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 function pariDispari(somma) {
@@ -37,9 +22,13 @@ function pariDispari(somma) {
     return "dispari";
 }
 
-
-
 function randomNumber1to5() {
     var risultato = Math.floor(Math.random()*5)+1;
     return risultato;
 }
+
+
+console.log(sceltaUtente);
+console.log(numeroUtente);
+console.log(numeroComputer);
+console.log(risultato);
